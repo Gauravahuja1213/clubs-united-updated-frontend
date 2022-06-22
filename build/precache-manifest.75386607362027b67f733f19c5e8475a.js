b00c9d5ec594a50b59f0760dd3f2e17f0ae0444f
gauravahuja1213 <gauravahuja1213@gmail.com>
2022-06-22 06:16:57 +0530

    added build version

---
+++ build/precache-manifest.75386607362027b67f733f19c5e8475a.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "c458c65c018af5e72aa1",
+    "url": "/static/css/main.512987d0.chunk.css"
+  },
+  {
+    "revision": "c458c65c018af5e72aa1",
+    "url": "/static/js/main.c458c65c.chunk.js"
+  },
+  {
+    "revision": "d6f59f5fc527a1110c64",
+    "url": "/static/js/1.d6f59f5f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0561d2f7428c1d9c2ed24789b92eb3f2",
+    "url": "/static/media/svnit logo.0561d2f7.jpg"
+  },
+  {
+    "revision": "74656f5552f616a9af8fca61d7b59dad",
+    "url": "/static/media/acm.74656f55.jfif"
+  },
+  {
+    "revision": "4c351c8392f1083e11a7170491a9566c",
+    "url": "/static/media/ces.4c351c83.jfif"
+  },
+  {
+    "revision": "e79c7971207191c1f764c0ab129baa15",
+    "url": "/static/media/cev.e79c7971.jfif"
+  },
+  {
+    "revision": "cc7302ff8d320565c62f829796919a2a",
+    "url": "/static/media/Ches.cc7302ff.jfif"
+  },
+  {
+    "revision": "dccacb2341198480a66c4418c7ddd904",
+    "url": "/static/media/chrd.dccacb23.jfif"
+  },
+  {
+    "revision": "bab081eec6c67a4afa315f0a67a03ba9",
+    "url": "/static/media/drishti.bab081ee.jfif"
+  },
+  {
+    "revision": "1dc80a57249aac9d6e37896a77265f53",
+    "url": "/static/media/ees.1dc80a57.jfif"
+  },
+  {
+    "revision": "fb074ea8a1d5513388645ae41c51d98c",
+    "url": "/static/media/isf.fb074ea8.jfif"
+  },
+  {
+    "revision": "bb23c1ff1a33d944b751af61e2bc0905",
+    "url": "/static/media/lac.bb23c1ff.jfif"
+  },
+  {
+    "revision": "d307aca675608fde2736d28f418d9cb2",
+    "url": "/static/media/mlsa.d307aca6.jfif"
+  },
+  {
+    "revision": "7291bf6858ffcdfe824a2b45392ad604",
+    "url": "/static/media/ncc.7291bf68.jfif"
+  },
+  {
+    "revision": "4657ae1237739c3aac5252afa1cf138c",
+    "url": "/static/media/nss.4657ae12.jfif"
+  },
+  {
+    "revision": "becb9149fd719db46bfac8f3d719c466",
+    "url": "/static/media/physics.becb9149.jfif"
+  },
+  {
+    "revision": "cfb74c782221f4010c708c442f10e257",
+    "url": "/static/media/sae.cfb74c78.jfif"
+  },
+  {
+    "revision": "30de3e5b514ab06cf4cfc3e1debafcc1",
+    "url": "/static/media/scosh.30de3e5b.jfif"
+  },
+  {
+    "revision": "5a87ec227d32f3b2f9268c0ed34e2217",
+    "url": "/static/media/spicmacy.5a87ec22.jfif"
+  },
+  {
+    "revision": "ad79e4d06e0baaa0daa7721bef739ab3",
+    "url": "/static/media/tech converge.ad79e4d0.jfif"
+  },
+  {
+    "revision": "9e6987361ace0f6c3a2779d84b9309f5",
+    "url": "/static/media/SVNIT_bg image.9e698736.jpg"
+  },
+  {
+    "revision": "be1b5be2b8525228ceb1c257d00df269",
+    "url": "/index.html"
+  }
+];
